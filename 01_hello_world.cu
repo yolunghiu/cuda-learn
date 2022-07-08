3b915612c1a4acb8ecb9030721726437626fd43d
liuhy <190978752@qq.com>
2022-07-08 17:43:39 +0800

    commit

--- 01_hello_world.cu
+++ 01_hello_world.cu
@@ -3,16 +3,17 @@
 // __global__告诉编译器这个是个可以在设备上执行的核函数
 __global__ void hello_world(void)
 {
-    printf("GPU: Hello world!\n");
+    printf("GPU: Hello world from GPU thread %d!\n", threadIdx.x);
 }
 
 int main(int argc, char** argv)
 {
     printf("CPU: Hello world!\n");
-    hello_world<<<1, 3>>>();
+    hello_world<<<1, 10>>>();
 
     // 等GPU执行完了，再退出主机线程
     // if no this line ,it can not output hello world from gpu
-    cudaDeviceReset();
+//    cudaDeviceReset();
+    cudaDeviceSynchronize();
     return 0;
 }
